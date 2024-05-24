411d360944bb08d585b0dbe3cbbd1c481e3477fb
BendEye <85690078+BendEye@users.noreply.github.com>
2024-05-24 12:30:08 -0400

    Update Pirate.java

--- Pirate/src/main/java/me/systems/bendeye/pirate/Pirate.java
+++ Pirate/src/main/java/me/systems/bendeye/pirate/Pirate.java
@@ -1,5 +1,9 @@
 package me.systems.bendeye.pirate;
 
+
+import com.retrooper.packetevents.PacketEvents;
+import com.retrooper.packetevents.packettype.PacketType;
+import com.retrooper.packetevents.packetwrappers.in.useentity.WrappedPacketInUseEntity;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Pirate extends JavaPlugin {
@@ -10,7 +14,9 @@ FiberManager fiberManager = new FiberManager();
     @Override
     public void onEnable() {
         // Plugin startup logic
+        PacketEvents.getSettings().setServer(this).setGlobalClick(true).setPlayerEventListener(new PacketListener()).inject();
         fiberManager.init();
+        
     }
 
     @Override
