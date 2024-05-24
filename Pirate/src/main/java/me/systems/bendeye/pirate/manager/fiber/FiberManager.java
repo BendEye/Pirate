c0e6f20bae9cab43330fc1c3a277868b0438231a
BendEye <85690078+BendEye@users.noreply.github.com>
2024-05-24 12:16:50 -0400

    Update FiberManager.java

--- Pirate/src/main/java/me/systems/bendeye/pirate/manager/fiber/FiberManager.java
+++ Pirate/src/main/java/me/systems/bendeye/pirate/manager/fiber/FiberManager.java
@@ -1,16 +1,6 @@
 package me.systems.bendeye.pirate.manager.fiber;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Flow;
-import java.util.concurrent.SubmissionPublisher;
-
-import java.io.File;
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
-import java.net.URL;
 import java.util.ArrayList;
-import java.util.Enumeration;
 import java.util.List;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.TimeUnit;
@@ -18,22 +8,16 @@ import java.util.concurrent.TimeUnit;
 public class FiberManager {
 
     public static void main(String[] args) {
-        List<Class<?>> classes = getClasses("me.systems.bendeye.checks.impl"); // CHECKS
         ForkJoinPool pool = new ForkJoinPool();
 
-        for (Class<?> clazz : classes) {
-            pool.execute(() -> {
-                try {
-                    Object instance = clazz.getDeclaredConstructor().newInstance();
-                    if (instance instanceof Runnable) {
-                        ((Runnable) instance).run();
-                    } else {
-                        System.err.println(clazz.getName() + " does not implement Runnable.");
-                    }
-                } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
-                    e.printStackTrace();
-                }
-            });
+        // Create a list of checks
+        List<Check> checks = new ArrayList<>();
+        checks.add(new Check("Speed Hack", "Detects players moving too fast", 10, "/ban {player}", 1));
+        checks.add(new Check("Suspicious Movement", "Detects players suddenly stopping movement", 5, "/warn {player}", 2));
+
+        // Execute custom anti-cheat checks in their own fibers
+        for (Check check : checks) {
+            pool.execute(() -> check.execute());
         }
 
         pool.shutdown();
@@ -44,43 +28,17 @@ public class FiberManager {
         }
     }
 
-    private static List<Class<?>> getClasses(String packageName) {
-        List<Class<?>> classes = new ArrayList<>();
-        try {
-            ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-            String path = packageName.replace('.', '/');
-            Enumeration<URL> resources = classLoader.getResources(path);
-            List<File> dirs = new ArrayList<>();
-            while (resources.hasMoreElements()) {
-                URL resource = resources.nextElement();
-                dirs.add(new File(resource.getFile()));
-            }
-            for (File directory : dirs) {
-                classes.addAll(findClasses(directory, packageName));
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return classes;
-    }
+    static class Check {
+        private CheckInfo checkInfo;
 
-    private static List<Class<?>> findClasses(File directory, String packageName) throws ClassNotFoundException {
-        List<Class<?>> classes = new ArrayList<>();
-        if (!directory.exists()) {
-            return classes;
+        public Check(String name, String description, int threshold, String action, int priority) {
+            this.checkInfo = new CheckInfo(name, description, threshold, action, priority);
         }
-        File[] files = directory.listFiles();
-        if (files == null) {
-            return classes;
+
+        public void execute() {
+            AntiCheatChecker antiCheatChecker = new AntiCheatChecker();
+            antiCheatChecker.check(this.checkInfo);
         }
-        for (File file : files) {
-            if (file.isDirectory()) {
-                assert !file.getName().contains(".");
-                classes.addAll(findClasses(file, packageName + "." + file.getName()));
-            } else if (file.getName().endsWith(".class")) {
-                classes.add(Class.forName(packageName + '.' + file.getName().substring(0, file.getName().length() - 6)));
-            }
-        }
-        return classes;
     }
 }
+
