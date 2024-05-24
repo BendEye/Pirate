be185d464825c8fa348720e20ed7279c398d49c5
BendEye <85690078+BendEye@users.noreply.github.com>
2024-05-24 12:25:56 -0400

    Update Pirate.java

--- Pirate/src/main/java/me/systems/bendeye/pirate/Pirate.java
+++ Pirate/src/main/java/me/systems/bendeye/pirate/Pirate.java
@@ -4,10 +4,13 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Pirate extends JavaPlugin {
 
+
+FiberManager fiberManager = new FiberManager();
+    
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        fiberManager.init();
     }
 
     @Override
